46fb46e7d529dbb34898d303351eb55bc077347f
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-15 23:08:13 -0400

    Not sure which project is submitted after just doing a whole new project

--- users.js
+++ users.js
@@ -28,22 +28,5 @@ export let users = [
 }
 ];
 
-// router.get('/', (req, res)=>{
-//    // console.log(`Hello Harlem!`);
-//     res.send('Hello Harlem!');
-//     console.log(users);
-    
-//     res.send(users);
-// });
-
-// router.post('/', (req, res)=>{
-//     console.log(`West-Side`);
-//     //console.log(req.body);
-//     const user = req.body//This is where my post is being stored. I can push my post into the array below
-//     users.push(user);//This is to add posts/objects/users to my array.
-
-//     res.send(`Hey ${user.firstName}${user.lastName}! Where is the next fight for freedom?`);//testing post requests and variables from user
-// });
-
 
 export default router;
