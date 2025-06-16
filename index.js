af774bb98139c01a419f425e0450f4da51481193
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-15 22:33:48 -0400

    I fixed my post issue by moving code from one file to the next

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+
 app.get('/', (req, res)=>{
     res.render(`views`,{users});
 });
@@ -17,7 +18,22 @@ app.get('/users', (req, res)=>{
     res.send({users});
 } )
 
+app.get('/', (req, res)=>{
+   // console.log(`Hello Harlem!`);
+    res.send('Hello Harlem!');
+    console.log(users);
+    
+    res.send(users);
+});
 
+app.post('/', (req, res)=>{
+    console.log(`West-Side`);
+    //console.log(req.body);
+    const user = req.body//This is where my post is being stored. I can push my post into the array below
+    users.push(user);//This is to add posts/objects/users to my array.
+
+    res.send(`Hey ${user.firstName}${user.lastName}! Where is the next fight for freedom?`);//testing post requests and variables from user
+});
 app.listen(port, (error)=>{
     if(!error){
         console.log(`Server is Running on port: ${port}`)
@@ -33,7 +49,7 @@ app.patch('/users/:id', (req, res)=>{
 
     Object.assign(superHeroToUpdate, req.body)//merge objects value of req.body will merge with superheroToUpdate changing its property value
     users[index] = superHeroToUpdate;// after merging req.body to superHerToUpdate put it back into it's index position.
-    FileSystem.writeFile('users/:id', JSON.stringigy(users), (err)=>{
+    fs.writeFile('users/:id', JSON.stringigy(users), (err)=>{
         res.status(200).json({
             status: "success",
             data: {
