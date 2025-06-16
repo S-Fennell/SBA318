e305d0dd08f95ac4943e17e8f29e69f073799242
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-15 20:27:22 -0400

    third commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import express from 'express';
+import { users} from './users.js';
+
---
+++ users.js
@@ -0,0 +1,26 @@
+export let users = [
+    {
+    id:1,
+    fName: 'Captain',
+    lName: 'America',
+    age: 80
+},
+{
+    id:2,
+    fName: 'Black',
+    lName: 'Widow',
+    age: 32
+},
+{
+    id:3,
+    fName: 'Bruce',
+    lName: 'Banner',
+    age: 35
+},
+{
+    id:4,
+    fName: 'Iron',
+    lName: 'Man',
+    age: 35
+}
+];
